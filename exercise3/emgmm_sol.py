28073f11b9933b05740234666efa3a5ab82670d7
Unknown <dhorvath19@gmail.com>
2019-03-15 19:06:22 +0100

    Added cross validation

--- exercise3/emgmm_sol.py
+++ exercise3/emgmm_sol.py
@@ -16,46 +16,66 @@ def plot_normal(mu, Sigma):
 
 ## Load data
 data = loadmat('clusterdata2d.mat')['data']
-N, D = data.shape
+
+data = data[:20]  # taking only a small part for testing
+
+num_data, dim = data.shape
 
 ## Initialize parameters
-K = 3  # try with different parameters
-mu = []
-Sigma = []
-pi_k = np.ones(K) / K
-for _ in range(K):
-    # Let mu_k be a random data point:
-    mu.append(data[np.random.choice(N)])
-    # Let Sigma_k be the identity matrix:
-    Sigma.append(np.eye(D))
+# K = 3  # try with different parameters
+# mu = []
+# for _ in range(num_data):
+#      Let mu_k be a random data point:
+#      mu.append(data[np.random.choice(num_data)])
+#     # Let Sigma_k be the identity matrix:
+#     Sigma.append(np.eye(D))
 
 ## Loop until you're happy
 max_iter = 100  # XXX: you should find a better convergence check than a max iteration counter
-log_likelihood = np.zeros(max_iter)
-respons = np.zeros((K, N))  # KxN
-for iteration in range(max_iter):
-    ## Compute responsibilities
-    # XXX: FILL ME IN!
-    for k in range(K):
-        respons[k] = pi_k[k] * multivariate_normal.pdf(data, mean=mu[k], cov=Sigma[k])
-    respons /= np.sum(respons, axis=0)
+sigmas = np.zeros(num_data)
 
-    ## Update parameters
-    # XXX: FILL ME IN!
-    for k in range(K):
-        respons_k = respons[k]  # N
-        Nk = np.sum(respons_k)  # scalar
-        mu[k] = respons_k.dot(data) / Nk  # D
-        delta = data - mu[k]  # NxD
-        Sigma[k] = (respons_k * delta.T).dot(delta) / Nk  # DxD
-        pi_k[k] = Nk / N
+for idx, test in enumerate(data):
+    if idx == 0:
+        train_array = data[1:]
+    if idx == num_data:
+        train_array = data[:-1]
+    if 0 < idx < num_data:
+        train_array = np.concatenate((data[:idx], data[idx + 1:]), axis=0)  # removing the current element
 
-    ## Compute log-likelihood of data
-    # log_likelihood[iteration] = -1 # XXX: DO THIS CORRECTLY
-    L = 0
-    for k in range(K):
-        L += pi_k[k] * multivariate_normal.pdf(data, mean=mu[k], cov=Sigma[k])
-    log_likelihood[iteration] = np.sum(np.log(L))
+    num_train = train_array.shape[0]
+
+    _sigma = [np.eye(dim) for _ in range(num_train)]
+    pi = (1.0 / num_train) * np.ones(num_train)  # same pi for each distribution
+    responsibility = np.zeros(num_train)
+    log_likelihood = np.zeros(max_iter)
+
+    for iteration in range(max_iter):
+        ## Compute responsibilities
+        for k, train in enumerate(train_array):
+            responsibility[k] = pi[k] * multivariate_normal.pdf(test, mean=train, cov=_sigma)
+        responsibility /= np.sum(responsibility, axis=0)
+
+        ## Update parameters
+        for k, train in enumerate(train_array):
+            responsibility_k = responsibility[k]  # num_train
+            Nk = np.sum(responsibility_k)  # scalar
+            # mu[k] = responsibility_k.dot(test) / Nk  # dim
+            delta = (test - train)[np.newaxis]  # NxD
+            _sigma = (responsibility_k * delta.T).dot(delta) / Nk  # dim x dim
+            pi[k] = Nk / num_train
+
+        ## Compute log-likelihood of data
+        # log_likelihood[iteration] = -1 # XXX: DO THIS CORRECTLY
+        L = 0
+        # print(_sigma)
+        for k, train in enumerate(train_array):
+            L += pi[k] * multivariate_normal.pdf(test, mean=train, cov=_sigma)
+        log_likelihood[iteration] = np.sum(np.log(L))
+    sigmas[idx] = _sigma
+    print('Run ' + str(idx))
+    print(_sigma)
+
+sigma = sigmas.mean(axis=1)
 
 ## Plot log-likelihood -- did we converge?
 plt.figure(1)
@@ -65,12 +85,14 @@ plt.ylabel('Log-likelihood')
 
 ## Plot data
 plt.figure(2)
-if D == 2:
+if dim == 2:
     plt.plot(data[:, 0], data[:, 1], '.')
-if D == 3:
+if dim == 3:
     plt.plot3(data[:, 0], data[:, 1], data[:, 2], '.')
 
-for k in range(K):
-    plot_normal(mu[k], Sigma[k])
+for _data in data:
+    print(_data)
+    print(sigma)
+    plot_normal(_data, sigma)
 
 plt.show()
