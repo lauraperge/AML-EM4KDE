2a3be799781757e3a2aa70aea0b41ce3c5cf0de1
Lorant Gulyas <lorant.gulyas@gmail.com>
2019-05-05 17:50:55 +0200

    Simplified_KDE with D-kernels and S-kernels

---
+++ EM_KDE_vs_simple_KDE_imp/EM_KDE_impute.py
@@ -0,0 +1,128 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+from sklearn import model_selection
+
+from utils import remove_random_value, remove_dim, conditional_expectation, e_step, m_step, \
+    calculate_log_likelihood, is_converged
+from plot import plot_kde
+
+## Load data
+data = loadmat('../faithfull/faithful.mat')['X']
+
+## Testing with higher dimension data
+# np.random.shuffle(data)
+# data = np.concatenate([data, loadmat('../faithfull/faithful.mat')['X']], axis=1)
+
+raw_data = data  # taking only a small part for testing
+data = np.array(raw_data[:-10])
+[damaged_data, removed_values] = remove_random_value(raw_data[-10:])
+
+num_data, dim = data.shape
+
+# K-fold cross validation
+K = num_data
+CV = model_selection.KFold(n_splits=K, shuffle=False)
+
+## Loop until you're happy
+epsilon = 1e-3
+sigma = np.eye(dim)
+log_likelihood = np.asarray([])
+i = 0
+while True:
+    i += 1
+    sigmas = []
+
+    R = np.linalg.cholesky(sigma)
+    A = data.dot(np.linalg.inv(R).T)
+
+    for train_index, test_index in CV.split(A):
+        # extract training and test set for current CV fold
+        a_test = A[test_index, :]
+        a_train = A[train_index, :]
+
+        x_test = data[test_index, :]
+        x_train = data[train_index, :]
+
+        # E step
+        responsibility = e_step(a_test, a_train, R)
+
+        # M step
+        sigmas.append(m_step(x_test, x_train, responsibility))
+
+    sigma = np.array(sigmas).sum(axis=1).mean(axis=0)
+
+    R = np.linalg.cholesky(sigma)
+    A = data.dot(np.linalg.inv(R).T)
+
+    _log_likelihood = []
+    for train_index, test_index in CV.split(A):
+        # extract training and test set for current CV fold
+        x_train = A[train_index, :]
+        x_test = A[test_index, :]
+
+        _log_likelihood.append(calculate_log_likelihood(x_test, x_train, R))
+
+    log_likelihood = np.append(log_likelihood, np.asarray(_log_likelihood).mean())
+
+    if is_converged(log_likelihood, epsilon):
+        break
+
+plt.figure(1)
+plt.plot(log_likelihood)
+plt.xlabel('Iterations')
+plt.ylabel('Log-likelihood')
+plt.show()
+
+# '''
+#
+# sigma = [[4.28747436e-02, 2.92396851e-01, 2.46394066e-04, 1.05465785e-01],
+#          [2.92396851e-01, 1.44238149e+01, 4.95674770e-02, -1.75754718e+00],
+#          [2.46394066e-04, 4.95674770e-02, 5.51668545e-02, 2.07264980e-01],
+#          [1.05465785e-01, -1.75754718e+00, 2.07264980e-01, 1.57786340e+01]]
+#
+# '''
+#
+# sigma = [[0.0322203, 0.0194771],
+#          [0.0194771, 3.8548159]]
+#
+# '''
+
+imputed_values = []
+restored_data = []
+for test_data in damaged_data:
+    # get index of missing dimension
+    missing_dim = [idx for idx, value in enumerate(test_data) if np.isnan(value)][0]
+
+    # remove data of that dimension
+    train_data = data
+    reduced_train_data = np.delete(train_data, missing_dim, axis=1)
+    test_data = np.delete(test_data, missing_dim, axis=0)
+    reduced_sigma = remove_dim(sigma, missing_dim)
+
+    # create transformed data
+    R = np.linalg.cholesky(reduced_sigma)
+    R_inv_T = np.linalg.inv(R).T
+    a_train = reduced_train_data.dot(R_inv_T)
+    a_test = test_data.dot(R_inv_T)
+
+    responsibility = np.squeeze(e_step(a_test, a_train, R))
+    cond_exp = np.array([conditional_expectation(mean, test_data, sigma, missing_dim) for mean in train_data])
+    imputed_value = np.sum(np.multiply(cond_exp, responsibility))
+
+    imputed_values.append(imputed_value)
+    restored_element = np.insert(test_data, missing_dim, imputed_value)
+    restored_data.append(restored_element)
+
+restored_data = np.array(restored_data)
+imputed_values = np.array(imputed_values)
+
+divergence = np.abs(removed_values - imputed_values) / removed_values
+
+plt.figure(2)
+plt.plot(divergence)
+plt.xlabel('Index')
+plt.ylabel('Imputation error in %')
+plt.show()
+
+plot_kde(data, sigma, 0.1)
---
+++ EM_KDE_vs_simple_KDE_imp/utils.py
@@ -0,0 +1,247 @@
+import random
+
+from scipy.linalg import eig
+from scipy.io import loadmat
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+
+
+def e_step(a_test, a_train, R):
+    num_test, num_train = len(a_test), len(a_train)
+
+    responsibility = np.zeros([num_train, num_test])
+    pi = 1.0 / num_train
+
+    for k, train in enumerate(a_train):
+        for j, test in enumerate(a_test):
+            responsibility[k, j] = pi * custom_normal_pdf(test, mean=train, R=R)
+    responsibility /= np.sum(responsibility, axis=0)
+
+    return responsibility
+
+
+def m_step(x_test, x_train, responsibility):
+    num_train, dim = x_train.shape
+    num_test = len(x_test)
+
+    sigmas = np.zeros([num_train, dim, dim])
+
+    for k, train in enumerate(x_train):
+        _sigmas = np.zeros([num_test, dim, dim])
+        for n, test in enumerate(x_test):
+            # Use these two lines to reproduce F-kernel, D-kernel_v1 and S-Kernel_v1
+            # delta = (test - train)[np.newaxis]
+            # _sigmas[n] = (responsibility[k, n] * delta.T).dot(delta)
+
+            # Use this line to reproduce D-Kernel_v2
+            _sigmas[n] = np.eye(dim) * (responsibility[k, n] * (test-train)**2)
+
+            # Use this line to reproduce S-Kernel_v2
+            # _sigmas[n] = np.eye(dim) * responsibility[k, n] * np.linalg.norm(test-train)
+
+        # Use this line to reproduce F-kernel, D-kernel_v2 and S-Kernel_v2
+        sigmas[k] = _sigmas.mean(axis=0)
+
+        # Use this line to reproduce D-Kernel_v1
+        # sigmas[k] = diagonalize(_sigmas.mean(axis=0))
+
+        # Use this line to reproduce S-Kernel_v1
+        # sigmas[k] = scalarize(_sigmas.mean(axis=0))
+    return sigmas
+
+
+def calculate_log_likelihood(x_test, x_train, R):
+    num_test, num_train = len(x_test), len(x_train)
+
+    L = np.zeros([num_test, num_train])
+    pi = 1.0 / num_train
+    for j, test in enumerate(x_test):
+        for k, train in enumerate(x_train):
+            L[j, k] = pi * custom_normal_pdf(test, mean=train, R=R)
+            # L[j, k] = pi * multivariate_normal.pdf(test, mean=train, cov=R)
+    return L.sum()
+
+
+def is_converged(log_likelihood, epsilon):
+    i = len(log_likelihood)
+    if i > 1:
+        change = log_likelihood[-1] / log_likelihood[-2] - 1.
+        print('Run {}, log likelihood: {}, change: {}'.format(i, log_likelihood[-1], change))
+
+        # if the Sigma matrix is diagonalized the loglikelihood has a tendency to overshoot
+        if i > 2:
+            old_change = log_likelihood[-2] / log_likelihood[-3] - 1.
+            new_change = log_likelihood[-1] / log_likelihood[-2] - 1.
+            if new_change > old_change:
+                print('Run {}, log likelihood: {}'.format(i, log_likelihood[-1]))
+                print('Finished.')
+                return True
+
+        if log_likelihood[-1] < log_likelihood[-2]:
+            print('Error: Log likelihood decreases.')
+            return True
+
+        if change < epsilon:
+            print('Finished.')
+            return True
+    else:
+        print('Run {}, log likelihood: {}'.format(i, log_likelihood[-1]))
+        return False
+
+
+## custom mv. norm pdf
+def custom_normal_pdf(a, mean, R):
+    """Multivariate Normal (Gaussian) probability density function with custom implementation. 
+ 
+    Parameters 
+    ---------------------------------- 
+        a : array_like 
+            Quantiles, with the last axis of `x` denoting the components. (In our implementation a_n for test data) 
+         
+        mean : array_like 
+            Mean of distribution. (In our implementation a_k for train data) 
+         
+        cov_lower_triangle : array_like
+            Lower triangle matrix of covariance of distribution. (In our implementation R) 
+     
+    Returns 
+    --------------------------------- 
+         pdf : ndarray or scalar
+            Probability density function evaluated at `a` 
+ 
+    """
+    if R.ndim < 2:
+        dim = 1
+    else:
+        dim = R.shape[0]
+
+    PI2R = (2 * np.pi) ** (dim / 2) * np.linalg.det(R)
+
+    if a.ndim == 0:
+        a = a[np.newaxis]
+    elif a.ndim == 1:
+        if dim == 1:
+            a = a[:, np.newaxis]
+        else:
+            a = a[np.newaxis, :]
+    # pdf = (1 / PI2R) * (np.exp(- 0.5 * (np.sum(np.power(a, 2), 1) - 2 *
+    #                                     a.dot(mean.T) + np.sum(np.power(mean, 2)))))
+
+    distance = a - mean
+    pdf = (1 / PI2R) * np.exp(- 0.5 * (distance.dot(distance.T)))
+
+    return np.squeeze(pdf)
+
+
+def remove_random_value(data_array):
+    num_data, dim = data_array.shape
+    removed_values = []
+
+    def remove_random(item):
+        i = int(round(random.random() * dim - 1))
+        removed_values.append(item[i])
+        item[i] = None
+        return item
+
+    damaged_data = np.array([remove_random(data) for data in data_array])
+    removed_values = np.array(removed_values)
+
+    return [damaged_data, removed_values]
+
+
+def remove_dim(sigma, dim):
+    reduced_sigma = np.delete(sigma, dim, axis=0)
+    reduced_sigma = np.delete(reduced_sigma, dim, axis=1)
+    return reduced_sigma
+
+
+def conditional_expectation(mean, test, sigma, dim):
+    # S11 = remove_dim(sigma, dim)
+    S22_inv = 1 / sigma[dim][dim]
+    S12 = np.delete(sigma[dim], dim, axis=0)[np.newaxis].T
+
+    m1 = mean[dim]
+    m2 = np.delete(mean, dim, axis=0)
+
+    return np.squeeze(m1 + S12.dot(S22_inv * (test - m2)))
+
+
+##############################################################################
+# Diagonalize the given covariance matrix, returning the
+# diagonal matrix W, and the unitary matrix V such that
+#  V * U * V^{-1} = W
+
+def diagonalize(cov_matrix):
+    """Diagonalize a given covariance matrix.
+
+        Parameters
+        ----------------------------------
+            cov_matrix : array_like
+                A covariance matrix where each element contains a non-zero value
+
+        Returns
+        ---------------------------------
+             matrix_W : array_like
+                Reduced covariance matrix where non-zero elements are located along the main axis
+                sigma = [[lambda1, 0, 0],[0, lambda2, 0],[0, 0, lambda3]]
+
+        """
+    (eig_vals, eig_vecs) = eig(cov_matrix)
+
+    # # Create the diagonalization matrix V
+    # matrix_V = np.array(eig_vecs)
+    # # Multiply V^{-1} * U * V to diagonalize
+    # matrix_W = np.dot(np.linalg.inv(matrix_V),np.dot(cov_matrix, matrix_V))
+
+    # Construct the diagonalized matrix that we want
+    matrix_diag = np.array(np.eye(len(cov_matrix)))
+    for i in range(len(eig_vals)):
+        matrix_diag[(i, i)] = eig_vals[i].real
+
+    # print(matrix_W, matrix_diag)
+
+    return matrix_diag
+
+
+def scalarize(cov_matrix):
+    """Acalarize a given covariance matrix.
+
+        Parameters
+        ----------------------------------
+            cov_matrix : array_like
+                A covariance matrix where each element contains a non-zero value
+
+        Returns
+        ---------------------------------
+             matrix_W : array_like
+                Reduced covariance matrix where non-zero elements are the same and they are located along the main axis
+                sigma = [[lambda, 0, 0],[0, lambda, 0],[0, 0, lambda]]
+
+
+        """
+    (eig_vals, eig_vecs) = eig(cov_matrix)
+
+    # Construct the scalarized matrix that we want
+    matrix_diag = np.array(np.eye(len(cov_matrix)))
+    for i in range(len(eig_vals)):
+        matrix_diag[(i, i)] = eig_vals[0].real
+
+    return matrix_diag
+
+
+if __name__ == '__main__':
+    a = np.array([5, 3])
+    b = np.array([8, 7])
+    #
+    # print(np.linalg.norm(a-b, ord=2, keepdims=True))
+    print(np.eye(2)*(a-b)**2)
+
+    delta = (a - b)[np.newaxis]
+    print((delta.T).dot(delta))
+
+    # sigma = [[3, 1, -1],
+    #          [1, 3, -1],
+    #          [-1, -1, 5]]
+    #
+    # scalarize(sigma)
